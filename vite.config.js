8f6263cfff3c647ce4f07e79fa9803708fe03f60
Andres-Hincapie-HRZ <elite.programmers.global@gmail.com>
2025-12-01 14:29:28 -0500

    Configuración para despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Modulo-Aulas/',
   server: {
     port: 3000, // Fija el puerto en 3000 (http://localhost:3000)
     open: true, // Abre el navegador automáticamente al iniciar
